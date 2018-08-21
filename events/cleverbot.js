7dfc6fa6e703d5a985fb7bca37e853dbd522bf1c
Tqcoz <33169104+Tqcoz@users.noreply.github.com>
2018-08-21 12:20:35 -0400

    Update cleverbot.js

--- events/cleverbot.js
+++ events/cleverbot.js
@@ -1,12 +1,12 @@
 module.exports = (bot, message, cb, prefix) => {
   if (message.author.bot) return;
   if (message.content.startsWith(prefix)) return;
-  cb.setNick("Hulkbot")
+  cb.setNick("Zanity Bot")
   if (!message.content.toLowerCase().includes("who is your creator")) {
         cb.create((err, session) => {
         cb.ask(message.content, (err, response) => {
         message.channel.send(response)
-        console.log(`${message.author.username}(${message.author.id}) said ${message.content}, and Hulkbot responded with ${response}!`)
+        console.log(`${message.author.username}(${message.author.id}) said ${message.content}, and ZanityBot responded with ${response}!`)
         if (err) {
           console.error(err)
         }
